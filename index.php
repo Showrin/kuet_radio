a27fd390df1adcf472497ce43762d1ae51426eff
Showrin <showrinece15@gmail.com>
2020-05-02 22:46:46 +0600

    Add authority levelwise access in home, onAir, team, alumni, schedule, contact page

--- index.php
+++ index.php
@@ -83,58 +83,96 @@
             ?>
               <li class="nav-item dropdown pl-sm-4">
                 <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="memberOptions"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="memberOptions"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
                 >
-                <img
+                  <img
                     src="./user_info/dp/<?php echo $user['dp_name']; ?>"
                     class="mr-2 card_img_thumbnail card_img_thumbnail--small rounded-circle"
-                    />
-                <?php echo $user['first_name']; ?>
+                  />
+                  <?php echo $user['first_name']; ?>
                 </a>
                 <div
-                class="dropdown-menu dropdown-menu-left"
-                aria-labelledby="memberOptions"
-                id="memberOptionsDropdownArea"
+                  class="dropdown-menu dropdown-menu-left"
+                  aria-labelledby="memberOptions"
+                  id="memberOptionsDropdownArea"
                 >
-                <a class="dropdown-item" href="./layouts/profile.php">Profile</a>
-                <a class="dropdown-item" href="./layouts/start_a_show.php"
-                    >Start a Show</a
-                >
-                <a class="dropdown-item" href="./layouts/comments.php">Comments</a>
-                <a class="dropdown-item" href="./layouts/running_show_settings.php"
-                    >Running Show Settings</a
-                >
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="./layouts/song_requests.php"
-                    >Song Requests</a
-                >
-                <a class="dropdown-item" href="./layouts/account_requests.php"
-                    >Account Requests</a
-                >
-                <a class="dropdown-item" href="./layouts/playlist_settings.php"
-                    >Playlist Settings</a
-                >
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="./layouts/schedule_settings.php"
-                    >Schedule Settings</a
-                >
-                <a class="dropdown-item" href="./layouts/server_settings.php"
-                    >Server Settings</a
-                >
-                <a class="dropdown-item" href="./layouts/member_management.php"
-                    >Member Management</a
-                >
-                <a class="dropdown-item" href="./layouts/committee_posts_settings.php"
-                    >Committee Posts Settings</a
-                >
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="./backend/logout.php">Logout</a>
+                  <a class="dropdown-item" href="./layouts/profile.php"
+                    >Profile</a
+                  >
+                  <?php
+                    if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator' || strtolower($user['authority_level']) === 'editor') {
+                  ?>
+                    <a class="dropdown-item" href="./layouts/start_a_show.php"
+                      >Start a Show</a
+                    >
+                    <a class="dropdown-item" href="./layouts/comments.php">Comments</a>
+                    <a class="dropdown-item" href="./layouts/running_show_settings.php"
+                      >Running Show Settings</a
+                    >
+                    <div class="dropdown-divider"></div>
+                  <?php
+                    }
+                  ?>
+                  <?php
+                    if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator') {
+                  ?>
+                    <a class="dropdown-item" href="./layouts/song_requests.php"
+                      >Song Requests</a
+                    >
+                  <?php
+                    }
+                  ?>
+                  <?php
+                    if(strtolower($user['authority_level']) === 'admin') {
+                  ?>
+                    <a class="dropdown-item" href="./layouts/account_requests.php"
+                      >Account Requests</a
+                    >
+                  <?php
+                    }
+                  ?>
+                  <?php
+                    if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator' || strtolower($user['authority_level']) === 'editor') {
+                  ?>
+                    <a class="dropdown-item" href="./layouts/playlist_settings.php"
+                      >Playlist Settings</a
+                    >
+                  <?php
+                    }
+                  ?>
+                  <?php
+                    if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator') {
+                  ?>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="./layouts/schedule_settings.php"
+                      >Schedule Settings</a
+                    >
+                  <?php
+                    }
+                  ?>
+                  <?php
+                    if(strtolower($user['authority_level']) === 'admin') {
+                  ?>
+                    <a class="dropdown-item" href="./layouts/server_settings.php"
+                      >Server Settings</a
+                    >
+                    <a class="dropdown-item" href="./layouts/member_management.php"
+                      >Member Management</a
+                    >
+                    <a class="dropdown-item" href="./layouts/committee_posts_settings.php"
+                      >Committee Posts Settings</a
+                    >
+                  <?php
+                    }
+                  ?>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="./backend/logout.php">Logout</a>
                 </div>
               </li>
             <?php
--- layouts/components/user_dropdown.php
+++ layouts/components/user_dropdown.php
@@ -1,3 +1,4 @@
+
 <li class="nav-item dropdown pl-sm-4">
   <a
   class="nav-link dropdown-toggle"
@@ -11,7 +12,7 @@
   <img
       src="../user_info/dp/<?php echo $user['dp_name']; ?>"
       class="mr-2 card_img_thumbnail card_img_thumbnail--small rounded-circle"
-      />
+  />
   <?php echo $user['first_name']; ?>
   </a>
   <div
@@ -19,37 +20,75 @@
   aria-labelledby="memberOptions"
   id="memberOptionsDropdownArea"
   >
-  <a class="dropdown-item" href="./profile.php">Profile</a>
-  <a class="dropdown-item" href="./start_a_show.php"
+  <a class="dropdown-item" href="./profile.php"
+      >Profile</a
+  >
+  <?php
+      if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator' || strtolower($user['authority_level']) === 'editor') {
+  ?>
+      <a class="dropdown-item" href="./start_a_show.php"
       >Start a Show</a
-  >
-  <a class="dropdown-item" href="./comments.php">Comments</a>
-  <a class="dropdown-item" href="./running_show_settings.php"
+      >
+      <a class="dropdown-item" href="./comments.php">Comments</a>
+      <a class="dropdown-item" href="./running_show_settings.php"
       >Running Show Settings</a
-  >
-  <div class="dropdown-divider"></div>
-  <a class="dropdown-item" href="./song_requests.php"
+      >
+      <div class="dropdown-divider"></div>
+  <?php
+      }
+  ?>
+  <?php
+      if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator') {
+  ?>
+      <a class="dropdown-item" href="./song_requests.php"
       >Song Requests</a
-  >
-  <a class="dropdown-item" href="./account_requests.php"
+      >
+  <?php
+      }
+  ?>
+  <?php
+      if(strtolower($user['authority_level']) === 'admin') {
+  ?>
+      <a class="dropdown-item" href="./account_requests.php"
       >Account Requests</a
-  >
-  <a class="dropdown-item" href="./playlist_settings.php"
+      >
+  <?php
+      }
+  ?>
+  <?php
+      if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator' || strtolower($user['authority_level']) === 'editor') {
+  ?>
+      <a class="dropdown-item" href="./playlist_settings.php"
       >Playlist Settings</a
-  >
-  <div class="dropdown-divider"></div>
-  <a class="dropdown-item" href="./schedule_settings.php"
+      >
+  <?php
+      }
+  ?>
+  <?php
+      if(strtolower($user['authority_level']) === 'admin' || strtolower($user['authority_level']) === 'moderator') {
+  ?>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="./schedule_settings.php"
       >Schedule Settings</a
-  >
-  <a class="dropdown-item" href="./server_settings.php"
+      >
+  <?php
+      }
+  ?>
+  <?php
+      if(strtolower($user['authority_level']) === 'admin') {
+  ?>
+      <a class="dropdown-item" href="./server_settings.php"
       >Server Settings</a
-  >
-  <a class="dropdown-item" href="./member_management.php"
+      >
+      <a class="dropdown-item" href="./member_management.php"
       >Member Management</a
-  >
-  <a class="dropdown-item" href="./committee_posts_settings.php"
+      >
+      <a class="dropdown-item" href="./committee_posts_settings.php"
       >Committee Posts Settings</a
-  >
+      >
+  <?php
+      }
+  ?>
   <div class="dropdown-divider"></div>
   <a class="dropdown-item" href="../backend/logout.php">Logout</a>
   </div>
