1bb7507e1945bb6a93ae29a1e461def3dfb9273b
Showrin <showrinece15@gmail.com>
2020-05-15 00:54:16 +0600

    Update get api response for sending songs in reverse order

--- api/get_songlist.php
+++ api/get_songlist.php
@@ -1,7 +1,8 @@
 <?php
   include "../backend/connect_db.php";
-  include "../backend/find_all_songs.php";
 
+  $query = "SELECT * FROM songs ORDER BY id DESC";
+  $songs = mysqli_query($connection, $query);
   $result = mysqli_fetch_all($songs, MYSQLI_ASSOC);
 
   echo json_encode($result);
