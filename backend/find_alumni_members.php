a17a81526f891755c3ea58bbb4ecd86bf7b1a4ad
Showrin <showrinece15@gmail.com>
2020-05-15 21:37:58 +0600

    Fix unapproved user showing in alumni page issue

--- backend/find_alumni_members.php
+++ backend/find_alumni_members.php
@@ -1,6 +1,6 @@
 <?php
 
-  $query = "SELECT * FROM users ORDER BY isInWorkingCommittee, start_year";
+  $query = "SELECT * FROM users WHERE isApproved = '1' ORDER BY isInWorkingCommittee, start_year";
   $team_members = mysqli_query($connection, $query);
 
 ?>
