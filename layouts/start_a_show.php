41a23b3af6fc3a9a0c72792741e7d5e037ecbeb7
Showrin <showrinece15@gmail.com>
2020-09-30 02:55:01 +0600

    Make the start-a-show button disabled while running a show

--- layouts/start_a_show.php
+++ layouts/start_a_show.php
@@ -433,7 +433,7 @@
 
             <div id="startShowFormSubmitBtn" class="form-row">
               <div class="col-12 mt-4 mb-3">
-                <button type="submit" class="btn btn-success">
+                <button type="submit" class="btn btn-success" <?php if(mysqli_num_rows($running_show)) { echo "disabled"; } ?>>
                   Start The Show
                 </button>
               </div>
