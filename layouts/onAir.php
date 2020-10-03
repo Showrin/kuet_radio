c13c3d4e7857ae5df6a805eb713be5e8fab4d31d
Showrin <showrinece15@gmail.com>
2020-10-03 10:46:16 +0600

    Add song_request module in all public pages

--- layouts/onAir.php
+++ layouts/onAir.php
@@ -200,80 +200,9 @@
     </div>
 
     <!-- request_a_song_modal starts -->
-    <div id="request_a_song_modal" class="modal fade" role="dialog">
-      <div class="modal-dialog modal-lg modal-dialog-centered" role="content">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title text_dark">Request a Song</h4>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              &times;
-            </button>
-          </div>
-          <div class="modal-body text-secondary">
-            <form>
-              <div class="form-group">
-                <label class="text_dark" for="song_name">Song Name</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="song_name"
-                  aria-describedby="songNameHelp"
-                  placeholder="Enter song name"
-                />
-                <small id="songNameHelp" class="form-text text-muted"
-                  >Please fill up this field</small
-                >
-              </div>
-              <div class="form-group">
-                <label class="text_dark" for="singer_name"
-                  >Singer Name <small><em>(Optional)</em></small></label
-                >
-                <input
-                  type="text"
-                  class="form-control"
-                  id="singer_name"
-                  aria-describedby="singerNameHelp"
-                  placeholder="Enter singer name"
-                />
-                <small id="singerNameHelp" class="form-text text-muted"
-                  >Please fill up this field</small
-                >
-              </div>
-              <div class="form-group">
-                <label class="text_dark" for="song_url"
-                  >Song Link/URL <small><em>(Optional)</em></small></label
-                >
-                <input
-                  type="text"
-                  class="form-control"
-                  id="song_url"
-                  aria-describedby="songURLHelp"
-                  placeholder="Enter song URL"
-                />
-                <small id="songURLHelp" class="form-text text-muted"
-                  >Please fill up this field</small
-                >
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="submit" class="btn btn-success">Send Request</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php
+      include './components/song_request_modal.php';
+    ?>
     <!-- request_a_song_modal ends -->
 
     <div class="container my-5">
