70f209c51ceab615c7c5d897e35f0d31929bee24
Showrin <showrinece15@gmail.com>
2020-10-04 22:46:58 +0600

    Setup mailer for contact page

---
+++ backend/send_mail.php
@@ -0,0 +1,29 @@
+<?php
+
+  $senderName = $_POST['senderName'];
+  $senderEmail = $_POST['senderEmail'];
+  $emailSubject = $_POST['emailSubject'];
+  $emailBody = $_POST['emailBody'];
+
+  $mail_to = "kuetradioofficial@gmail.com";
+  $mail_from = $senderEmail;
+
+  $headers = "From: $mail_to";
+  $headers .= "Reply-to: $mail_from";
+
+  $mail_subject = "[showrin.com] $emailSubject";
+  $mail_body = "$emailBody \n \nfrom \n$senderName \n$senderEmail";
+
+  if($senderName != '' && $senderEmail != '' && $emailSubject != '' && $emailBody != ''){
+    if (mail($mail_to, $mail_subject, $mail_body, $headers)) {
+      $mail_sent_successfully = true;
+    } else {
+      $mail_sent_successfully = false;
+    }
+  } else {
+    $mail_sent_successfully = false;
+    echo $mail_sent_successfully;
+    header("Location: " . $_SERVER['HTTP_REFERER'] . "#contact_form?mail_sent=$mail_sent_successfully");
+  }
+
+?>
