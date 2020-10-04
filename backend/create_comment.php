b4fef49d49f83752e2dd43cfa14fbcc3d2223d75
Showrin <showrinece15@gmail.com>
2020-10-04 08:35:31 +0600

    Testing timezone in remote server

--- backend/create_comment.php
+++ backend/create_comment.php
@@ -5,9 +5,12 @@
   $commentator_batch = mysqli_real_escape_string($connection, $_POST['commentator_batch']);
   $comment = mysqli_real_escape_string($connection, $_POST['comment']);
   $comment_timestamp = time() + (4 * 60 * 60);
+  $comment_timestamp = date('e');
+
+  echo $comment_timestamp . '<br />';
 
   $comment_insert_query = "INSERT INTO comments VALUES ('', '$commentator_name', '$commentator_batch', '$comment', '$comment_timestamp')";
   mysqli_query($connection, $comment_insert_query);
 
-  header("Location: " . $_SERVER['HTTP_REFERER']);
+  // header("Location: " . $_SERVER['HTTP_REFERER']);
 ?>
