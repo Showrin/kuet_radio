5ad3514d14a5704a0710ca326ba092abb6ea4bc5
Showrin <showrinece15@gmail.com>
2020-10-05 22:29:35 +0600

    Reorder the team members based on position, start_year and name

--- backend/find_team_members.php
+++ backend/find_team_members.php
@@ -1,6 +1,6 @@
 <?php
 
-  $query = "SELECT * FROM users WHERE isInWorkingCommittee = '1'";
+  $query = "SELECT * FROM users INNER JOIN committee_posts ON users.designation = committee_posts.post_name WHERE users.isInWorkingCommittee = '1' ORDER BY committee_posts.post_priority, users.start_year, users.first_name";
   $team_members = mysqli_query($connection, $query);
 
 ?>
