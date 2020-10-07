19cf53ab6432f8fd17f40b0cb09708ac8c6dcfa9
Showrin <showrinece15@gmail.com>
2020-10-07 07:08:22 +0600

    Show only former members in the alumni page

--- backend/find_alumni_members.php
+++ backend/find_alumni_members.php
@@ -1,6 +1,6 @@
 <?php
 
-  $query = "SELECT * FROM users INNER JOIN committee_posts ON users.designation = committee_posts.post_name WHERE users.isApproved = '1' ORDER BY users.start_year, committee_posts.post_priority, users.first_name";
+  $query = "SELECT * FROM users INNER JOIN committee_posts ON users.designation = committee_posts.post_name WHERE users.isApproved = '1' AND users.isInWorkingCommittee = '0' ORDER BY users.start_year, committee_posts.post_priority, users.first_name";
   $team_members = mysqli_query($connection, $query);
 
 ?>
