f1a28e11267da9f7d1f19ffded0decb91f63722e
Showrin <showrinece15@gmail.com>
2020-10-07 22:59:31 +0600

    Test mailer

--- backend/create_current_show.php
+++ backend/create_current_show.php
@@ -51,6 +51,7 @@
     }
     
     include "../backend/find_current_show.php";
+    include "../backend/send_mail_after_starting_show.php";
 
     // header("Location:../layouts/start_a_show.php");
 ?>
